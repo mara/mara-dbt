93f8bdd0e65b74c49036520a7f809b0c5b819df6
Leo Schick <leonhard.schick@gmail.com>
2022-12-02 09:45:31 +0100

    bump version 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.0 (2022-12-02)
+
+- add command `RunDbtJob` executing a dbt cloud job
+
 ## 0.1.0 (2022-10-22) 
 
 - Initial version
--- mara_dbt/__init__.py
+++ mara_dbt/__init__.py
@@ -1,5 +1,5 @@
 """Make the functionalities of this package auto-discoverable by mara-app"""
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 def MARA_CONFIG_MODULES():
